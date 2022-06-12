150b9a2c3dec4296a8655cd8d877fa394a4946a4
Erwan <erwan.bourdeau@gmail.com>
2022-06-12 17:46:32 -0400

    Refactor to manage multiple tyle types

--- app.js
+++ app.js
@@ -8,20 +8,24 @@ var server = http.createServer(app)
 var io=socketIO(server); 
 
 var player1 = {
-    name: "player1"
+    name: "player1",
 };
 var noPlayer1 = true;
 
 var player2 = {
-    name: "player2"
+    name: "player2",
 };
 var noPlayer2 = true;
+
+player1.enemy = player2;
+player2.enemy = player1;
 
 app.get("/", (req,res) => {
     res.send(fs.readFileSync("index.html").toString());
 });
   
 // make connection with user from server side 
+let players = [];
 io.on('connection', (socket)=>{ 
     let player;
     if(noPlayer1)
@@ -34,15 +38,32 @@ io.on('connection', (socket)=>{
         player = player2;
         noPlayer2 = false;
     }
+    player.socket = socket;
     console.log(player.name + " connected"); 
     
     socket.emit('name', player.name); 
     
     // listen for message from user 
-    socket.on('setBoat', (msg)=>{ 
-        player.boat = JSON.parse(msg);
-        console.log(player.boat);
+    socket.on('setBoard', (msg)=>{ 
+        player.board = JSON.parse(msg);
+        console.log(player.board);
     }); 
+
+    socket.on("play", (position)=>{
+        let pos = position.split(",");
+        console.log(pos, pos[0], pos[1]);
+        let x = parseInt(pos[0]);
+        let y = parseInt(pos[1]);
+        console.log(x,y);
+        let data = {
+            x: x,
+            y: y,
+            value: player.enemy.board[x][y]
+        };
+        console.log(data);
+        player.enemy.socket.emit("playerUpdate", data);
+        player.socket.emit("enemyUpdate", data);
+    })
     
   // when server disconnects from user 
     socket.on('disconnect', ()=>{ 
