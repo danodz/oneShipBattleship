bfe795e9140cc4a92adb666094b1603a6a709007
Erwan <erwan.bourdeau@gmail.com>
2022-06-14 13:51:32 -0400

    Added some gameState logic for setup and turns

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ var player2 = {
 };
 var noPlayer2 = true;
 
+var gameState = "setup";
+
 player1.enemy = player2;
 player2.enemy = player1;
 
@@ -45,21 +47,31 @@ io.on('connection', (socket)=>{
     
     // listen for message from user 
     socket.on('setBoard', (msg)=>{ 
-        player.board = JSON.parse(msg);
-        console.log(player.board);
+        if(gameState == "setup")
+        {
+            player.board = JSON.parse(msg);
+            if(player.enemy.board)
+            {
+                gameState = "player1"
+            }
+        }
     }); 
 
     socket.on("move", (position)=>{
-        let pos = position.split(",");
-        let x = parseInt(pos[0]);
-        let y = parseInt(pos[1]);
-        let data = {
-            x: x,
-            y: y,
-            value: player.enemy.board[x][y]
-        };
-        player.enemy.socket.emit("playerUpdate", data);
-        player.socket.emit("enemyUpdate", data);
+        if(gameState == player.name)
+        {
+            let pos = position.split(",");
+            let x = parseInt(pos[0]);
+            let y = parseInt(pos[1]);
+            let data = {
+                x: x,
+                y: y,
+                value: player.enemy.board[x][y]
+            };
+            player.enemy.socket.emit("playerUpdate", data);
+            player.socket.emit("enemyUpdate", data);
+            gameState = player.enemy.name;
+        }
     })
     
   // when server disconnects from user 
